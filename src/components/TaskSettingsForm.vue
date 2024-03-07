1b4d1ad7fcf989f6abab6b70f7102dac70d3babe
iaksenov <i.aksenov@nicetu.spb.ru>
2024-03-07 16:56:01 +0300

    component for adding tasks

---
+++ src/components/TaskSettingsForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-sheet 
+    class="pa-4 d-flex flex-column"
+    width="400px">
+    <VTextField label="Title"/>
+    <v-sheet width="100%">
+      <v-btn @click="closeWindow">Cancel</v-btn>
+      <v-btn @click="saveTask()">Save</v-btn>
+    </v-sheet>
+    
+  </v-sheet>
+</template>
+
+<script setup>
+import { defineEmits } from 'vue';
+
+const emit = defineEmits(['closeWindow'])
+
+
+const saveTask = () => {
+  
+  emit('closeWindow');
+}
+const closeWindow = () => {
+  emit('closeWindow');
+}
+</script>
