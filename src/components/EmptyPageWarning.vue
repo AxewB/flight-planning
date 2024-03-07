276baa129cc4ad4f47d3900321e09c3c4b6934fe
iaksenov <i.aksenov@nicetu.spb.ru>
2024-03-07 14:52:57 +0300

    created warning for empty pages or sections

---
+++ src/components/EmptyPageWarning.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-sheet 
+    class="d-flex flex-column justify-center align-center pa-2"
+    width="100%"
+    height="100%">
+    <v-sheet class="text-h6 mb-2"> 
+      {{ props.title }}
+    </v-sheet>
+    <v-sheet class="mb-2">
+      {{ props.text }}
+    </v-sheet>
+    <v-sheet class="d-flex flex-row justify-end">
+      <slot name="actions"></slot>  
+    </v-sheet>
+    
+  </v-sheet>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['title', 'text'])
+
+</script>
