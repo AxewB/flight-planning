b722a502ba2a8ab54456d11dd39e2c021a696cf9
AxewB <speedwow@gmail.com>
2024-03-12 23:12:24 +0300

    text to textField component

---
+++ src/components/TextToTextField.vue
@@ -0,0 +1,60 @@
+<!-- eslint-disable vue/html-indent -->
+<template>
+  <v-sheet 
+		class="bg-transparent d-flex justify-space-between" 
+		width="100%">
+    <div 
+      v-if="!isEditing"
+      class="overflow-hidden cursor-text"
+      @click="enableEditing()">
+      <v-alert
+        v-if="!modelValue"
+        icon="mdi-alert-circle" 
+        type="error"
+        :title="'No ' + props.replacableText"
+        :text="'Click me to edit ' + props.replacableText"
+        variant="tonal"
+      ></v-alert>
+      {{ modelValue ? modelValue : '' }}
+    </div>
+    <v-text-field 
+      v-else
+      autofocus
+      variant="solo-filled"
+			hide-details
+			density="compact"
+      @keydown.enter.prevent="disableEditing()"
+      v-click-outside="{
+        handler: disableEditing,
+      }"
+			v-model="modelValue"
+			@update:modelValue="$emit('update:modelValue', modelValue)"/>
+  </v-sheet>
+</template>
+<!-- eslint-disable indent -->
+<script setup>
+import { defineProps, ref } from 'vue';
+
+const props = defineProps({
+  text: {
+    type: String,
+  },
+	replacableText: {
+		type: String
+	},
+})
+
+const modelValue = ref(props.text);
+const isEditing = ref(false);
+
+const disableEditing = () => {
+	console.log('disabling editing')
+  isEditing.value = false;
+}
+const enableEditing = () => {
+	console.log('enabling editing')
+  isEditing.value = true;
+}
+
+
+</script>
