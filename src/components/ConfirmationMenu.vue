6cbd8e67402970d7fa71cf698cdae265a3f2dc51
iaksenov <i.aksenov@nicetu.spb.ru>
2024-03-14 14:26:50 +0300

    reusable one-button component for confirmation

---
+++ src/components/ConfirmationMenu.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-menu activator="parent">
+    <v-sheet class="pa-2">
+      <v-btn 
+        color="error"
+        @click="emit('OK')">Are you sure?</v-btn>
+    </v-sheet>
+  </v-menu>
+</template>
+
+<script setup>
+import { defineEmits } from 'vue'
+
+const emit = defineEmits(['OK'])
+</script>
