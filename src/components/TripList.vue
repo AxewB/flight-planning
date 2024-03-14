6de72e3905f6deb6760a5ddf2f16cf961930df0c
iaksenov <i.aksenov@nicetu.spb.ru>
2024-03-14 16:31:18 +0300

    added trip list display

---
+++ src/components/TripList.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-sheet v-if="tripStore.trips.length === 0">
+    <EmptyPageWarning 
+      title="No trips yet"
+      text="Create your first trip">
+      <template #actions>
+        <v-btn @click="tripStore.addTrip()">Create trip</v-btn>
+      </template>
+    </EmptyPageWarning>
+  </v-sheet>
+  <v-list lines="two">
+    <v-list-item 
+      v-for="trip in tripStore.trips" 
+      :key="trip.id"
+      @click="moveToTrip(trip.id)">
+      <template #prepend>
+        <VAvatar  
+          :image="trip.avatar.image"
+          :color="!trip.avatar.image ? trip.avatar.color : ''"
+          size="40"/>
+      
+      </template>
+      <v-list-item-title>{{ trip.title }}</v-list-item-title>
+      <v-list-item-subtitle>
+        <VIcon icon="mdi-cash"/> {{ trip.budget }}
+        <VIcon icon="mdi-calendar"/> {{ trip.date.begin }}
+      </v-list-item-subtitle>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script setup>
+import { useTripStore } from "@/stores/TripStore";
+import EmptyPageWarning from "@/components/EmptyPageWarning.vue";
+import { useRouter } from 'vue-router'
+const tripStore = useTripStore();
+const router = useRouter();
+const moveToTrip = (id) => {
+  router.push({name: "trip", params: {id}})
+}
+</script>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -4,10 +4,11 @@
     width="100%"
     height="100%"
   >
-    
+    <TripList/>
   </v-sheet>
 </template>
 
 <script setup>
+import TripList from '@/components/TripList.vue';
 
 </script>
