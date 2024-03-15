0afe58a53f1d2d45fc370f570fb097d935f6576b
iaksenov <i.aksenov@nicetu.spb.ru>
2024-03-15 15:44:52 +0300

    first look of main page

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -6,7 +6,15 @@
   >
     <v-container>
       <v-row>
+        <v-sheet class="py-8 px-4 text-h2">
+          👋 Hello, {{ userStore.firstName }}
+        </v-sheet>
+      </v-row>
+      <v-row>
         <v-col>
+          <v-sheet class="pa-4 text-h4">
+            Your closest trip
+          </v-sheet>
           <v-card
             class="mx-auto mb-4"
             max-width="400"
@@ -15,38 +23,44 @@
             <v-img
               class="align-end text-white"
               height="200"
-              :src="tripStore.closestTrip?.avatar.image"
+              :color="closestTrip.avatar.image ? '' : closestTrip.avatar.color"
+              :src="closestTrip.avatar.image"
               cover
             >
-              <v-card-title>Top 10 Australian beaches</v-card-title>
+              <v-card-title>{{ closestTrip.title }}</v-card-title>
             </v-img>
 
             <v-card-subtitle class="pt-4">
-              Number 10
+              <VIcon icon="mdi-calendar"/> {{ closestTrip.date.begin }} - {{ closestTrip.date.end }}
             </v-card-subtitle>
 
             <v-card-text>
-              <div>Whitehaven Beach</div>
-
-              <div>Whitsunday Island, Whitsunday Islands</div>
+              <div>{{ closestTrip.description }}</div>
             </v-card-text>
 
             <v-card-actions>
-              <v-btn color="orange">
-                Share
-              </v-btn>
-
-              <v-btn color="orange">
-                Explore
+              <v-btn 
+                color="primary"
+                @click="">
+                Open
               </v-btn>
             </v-card-actions>
           </v-card>
+        </v-col>
+        <VDivider vertical/>
+        <v-col>
+          <v-sheet class="pa-4 text-h4">
+            Upcoming trips
+          </v-sheet>
           <TripList/>
         </v-col>
+        <VDivider vertical/>
         <v-col>
-          <FriendList/>
+          <v-sheet class="pa-4 text-h4">
+            Friends
+          </v-sheet>
+          <FriendList short/>
         </v-col>
-        <v-col></v-col>
       </v-row>
     </v-container>
 
@@ -54,7 +68,16 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import TripList from '@/components/TripList.vue';
 import FriendList from '@/components/FriendList.vue';
-import {useTripStore}
+import { useTripStore } from "@/stores/TripStore"
+import { useUserStore } from "@/stores/UserStore"
+
+const tripStore = useTripStore();
+const userStore = useUserStore();
+
+const closestTrip = computed(() => {
+  return tripStore.closestTrip;
+})
 </script>
